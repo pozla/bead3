5eb7d7b9197b598f262eae74c12528d1249fa500
pozla <50776000+pozla@users.noreply.github.com>
2019-05-18 18:09:05 +0200

    Update "main"
    Remove lines from "main" referring to widgets not used in this project.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include "widget.hpp"
-#include "setnumberwidget.hpp"
-#include "chooselistwidget.hpp"
 #include "graphics.hpp"
 #include <vector>
 #include <string>
@@ -36,16 +34,6 @@ void eventLoop(vector<Widget*> &widgetVector) {
 int main() {
     gout.open(600,200);
     vector<Widget*> widgetVector;
-    SetNumberWidget *snw1 = new SetNumberWidget(25, 50, 75, gout.cascent()+gout.cdescent(), -25, 25);
-    SetNumberWidget *snw2 = new SetNumberWidget(25, 125, 100, gout.cascent()+gout.cdescent(), -50, 50);
-    vector<string> options1 {"Red", "Green", "Blue"};
-    vector<string> options2 {"Cyan", "Magenta", "Yellow", "Black", "Purple", "Orange", "White"};
-    ChooseListWidget *clw1 = new ChooseListWidget(200, 25, 75, 3*(gout.cascent()+gout.cdescent()), options1);
-    ChooseListWidget *clw2 = new ChooseListWidget(400, 25, 100, 5*(gout.cascent()+gout.cdescent()), options2);
-    widgetVector.push_back(snw1);
-    widgetVector.push_back(snw2);
-    widgetVector.push_back(clw1);
-    widgetVector.push_back(clw2);
     for(Widget *w: widgetVector) { w->draw(); }
     gout << refresh;
     eventLoop(widgetVector);
