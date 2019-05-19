01a993219e679d27931218c0a56880655daf19df
pozla <50776000+pozla@users.noreply.github.com>
2019-05-20 01:08:20 +0200

    Implement checker functions

--- main.cpp
+++ main.cpp
@@ -5,15 +5,54 @@
 #include <string>
 #include <fstream>
 
-#include <iostream>
-
 using namespace genv;
 using namespace std;
 
+class Rules {
+    public:
+        bool horizontalCheck(int gameData[20][20]) {
+            for (int i=0; i<20; i++) {
+                for (int j=0; j<16; j++) {
+                    if ((gameData[i][j] == 1) && (gameData[i][j+1] == 1) && (gameData[i][j+2] == 1) && (gameData[i][j+3] == 1) && (gameData[i][j+4] == 1) ||
+                        (gameData[i][j] == 2) && (gameData[i][j+1] == 2) && (gameData[i][j+2] == 2) && (gameData[i][j+3] == 2) && (gameData[i][j+4] == 2)) return true;
+                }
+            }
+            return false;
+        }
+        bool verticalCheck(int gameData[20][20]) {
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<20; j++) {
+                    if ((gameData[i][j] == 1) && (gameData[i+1][j] == 1) && (gameData[i+2][j] == 1) && (gameData[i+3][j] == 1) && (gameData[i+4][j] == 1) ||
+                        (gameData[i][j] == 2) && (gameData[i+1][j] == 2) && (gameData[i+2][j] == 2) && (gameData[i+3][j] == 2) && (gameData[i+4][j] == 2)) return true;
+                }
+            }
+            return false;
+        }
+        bool diagonalCheck1(int gameData[20][20]) {
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<16; j++) {
+                    if ((gameData[i][j] == 1) && (gameData[i+1][j+1] == 1) && (gameData[i+2][j+2] == 1) && (gameData[i+3][j+3] == 1) && (gameData[i+4][j+4] == 1) ||
+                        (gameData[i][j] == 2) && (gameData[i+1][j+1] == 2) && (gameData[i+2][j+2] == 2) && (gameData[i+3][j+3] == 2) && (gameData[i+4][j+4] == 2)) return true;
+                }
+            }
+            return false;
+        }
+        bool diagonalCheck2(int gameData[20][20]) {
+            for (int i=0; i<16; i++) {
+                for (int j=4; j<20; j++) {
+                    if ((gameData[i][j] == 1) && (gameData[i+1][j-1] == 1) && (gameData[i+2][j-2] == 1) && (gameData[i+3][j-3] == 1) && (gameData[i+4][j-4] == 1) ||
+                        (gameData[i][j] == 2) && (gameData[i+1][j-1] == 2) && (gameData[i+2][j-2] == 2) && (gameData[i+3][j-3] == 2) && (gameData[i+4][j-4] == 2)) return true;
+                }
+            }
+            return false;
+        }
+};
+
 void gameEngine(vector<CheckBoxWidget*> &widgetVector) {
+    Rules rules;
     int gameData[20][20];
     for (int i=0; i<20; i++) {
-        for (int j=0; j<20; j++) gameData[i][j] = 0;
+        for (int j=0; j<16; j++) gameData[i][j] = 0;
     }
     gout.open(600, 600);
     gout << color(100, 100, 100) << move_to(49, 49) << box(502, 502);
@@ -25,7 +64,7 @@ void gameEngine(vector<CheckBoxWidget*> &widgetVector) {
                 for (size_t i=0; i<widgetVector.size(); i++) {
                     if (widgetVector[i]->isSelected(ev.pos_x, ev.pos_y)) {
                         active = i;
-                        gameData[active/20+1][active%20+1] = 1;
+                        gameData[active/20][active%20] = 1;
                     }
                 }
             }
@@ -33,7 +72,7 @@ void gameEngine(vector<CheckBoxWidget*> &widgetVector) {
                 for (size_t i=0; i<widgetVector.size(); i++) {
                     if (widgetVector[i]->isSelected(ev.pos_x, ev.pos_y)) {
                         active = i;
-                        gameData[active/20+1][active%20+1] = 2;
+                        gameData[active/20][active%20] = 2;
                     }
                 }
             }
